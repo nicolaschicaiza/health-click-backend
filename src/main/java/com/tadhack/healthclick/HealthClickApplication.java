28cc6dffe46a606e715d7e753239ea327821332b
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-10-21 11:06:45 -0500

    :heavy_plus_sign: (NICO-1) Agregado proyecto de spring boot

---
+++ src/main/java/com/tadhack/healthclick/HealthClickApplication.java
@@ -0,0 +1,13 @@
+package com.tadhack.healthclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthClickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthClickApplication.class, args);
+	}
+
+}
